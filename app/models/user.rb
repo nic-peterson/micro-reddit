62f2f363c9a5a9d78df91ac8056b978ee2211f7c
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-04 16:37:55 -0800

    Added custom error messages to User Model validators.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username,
+            presence: { message: "Username can't be blank" },
+            uniqueness: { case_sensitive: false, message: "Username has already been taken" },
+            length: { maximum: 50, too_long: "Username can't be longer than 50 characters" }
+
+  validates :email,
+            presence: { message: "Email can't be blank" },
+            uniqueness: { message: "Email has already been taken" },
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email is not a valid format" }
 end
